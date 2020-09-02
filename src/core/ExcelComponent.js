ec8ac5b3775584963fda1b2e854b81a3ca926da8
kolianos007 <kolianos007@gmail.com>
2020-09-02 22:26:24 +0300

    Framework start (#4)
    * Finish base framework functional
    
    * comment debug
    
    Co-authored-by: Kol_ko <kolianos007@gmai.com>

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,23 @@
+/* eslint-disable require-jsdoc */
+import {
+  DomListener,
+} from './DomListener';
+
+export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners)
+    this.name = options.name || ''
+  }
+  // Возвращает шаблон компонента
+  toHTML() {
+    return ''
+  }
+
+  init() {
+    this.initDOMListeners()
+  }
+
+  destroy() {
+    this.removeDOMListeners
+  }
+}
